ec9ac032ff587405275714d1e032851ca80dd138
haaniki <annaanikeeva2020@gmail.com>
2026-08-07 22:06:48 +0200

    search errors

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+
 import Container from "../UI/Container/Container";
 import css from "./Footer.module.css";
 import { FaFacebookF } from "react-icons/fa";
@@ -8,28 +9,35 @@ export default function Footer() {
     <footer className={css.footer}>
       <Container>
         <div className={css.content}>
-
           <div className={css.brand}>
-          <img src={lotusLogo} alt="Az én gyöngyöm" className={css.logoIcon}/>
+            <img src={lotusLogo} alt="Az én gyöngyöm" className={css.logoIcon}/>
+            <h2 className={css.title}>Az én gyöngyöm</h2>
+            <a className={css.facebook} href="https://www.facebook.com/erzsebet.lajter.1"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Facebook">
+              <FaFacebookF />
+            </a>
           </div>
 
-          <h2 className={css.title}>
-            Az én gyöngyöm
-          </h2>
+          <div className={css.contacts}>
+            <h3>Kapcsolat</h3>
+            <a href="mailto:info@azenigyongyom.hu">info@azenigyongyom.hu</a>
+            <a href="tel:+3612345678">+36 12 345 678</a>
+            <p>Magyarország</p>
+          </div>
 
-          <p className={css.text}>
-            Kézzel készült karkötők és medálok szeretettel.
-          </p>
-
-          <a className={css.facebook} href="https://www.facebook.com/erzsebet.lajter.1" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-          <FaFacebookF />
-          </a>
-
-          <span className={css.copy}>
-            © 2026 Az én gyöngyöm
-          </span>
-
+          <div className={css.message}>
+            <p>Köszönöm, hogy meglátogattad
+              az oldalamat.
+            </p>
+            <p>
+              Minden ékszert szeretettel
+              és odafigyeléssel készítek.
+            </p>
+          </div>
         </div>
+        <div className={css.copy}>© 2026 Az én gyöngyöm</div>
       </Container>
     </footer>
   );
